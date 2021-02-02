193125db74f03559350fc59301f8bb0d598d1f5e
Konstantin <O_o@iMac-BadBudahh.local>
2021-02-02 14:52:03 +0200

    second commit

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,61 @@
 'use strict';
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMoviesDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMoviesDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const a = prompt('Последний просмотренный вами фильм?', ''),
-    b = prompt('На сколько вы его оцените', ''),
-    c = prompt('Последний просмотренный вами фильм?', ''),
-    d = prompt('На сколько вы его оцените', '');
+// const a = prompt('Последний просмотренный вами фильм?', ''),
+//     b = prompt('На сколько вы его оцените', ''),
+//     c = prompt('Последний просмотренный вами фильм?', ''),
+//     d = prompt('На сколько вы его оцените', '');
 
-    personalMoviesDB.movies[a] = b
-    personalMoviesDB.movies[c] = d
+//     personalMoviesDB.movies[a] = b
+//     personalMoviesDB.movies[c] = d
 
-console.log(personalMoviesDB);
+// console.log(personalMoviesDB);
+
+// const num = 151;
+
+// (num > 55) ? console.log('true') : console.log('false');
+// ======= Оператор switch
+// switch (num) {
+//     case 49:
+//         console.log('Error');
+//         break;
+//     case 100:
+//         console.log('Bingo')
+//         break;    
+//     case 151:
+//         console.log('Bullseye')
+//         break;
+//     default:
+//         console.log('miss again');        
+// }
+
+// let num = 50;
+
+// while(num < 55) {
+//     console.log(`Cont start: ${num}`);
+//     num++
+// }
+
+// do {
+//         console.log(`Cont start: ${num}`);
+//     num++;
+// } 
+// while (num < 60);
+
+// for(let i = 0; i < 8; i++) {
+//     if(i === 5) {
+//         console.log('Success');
+//         break;
+//     }
+//     console.log(`Nasa hacking: ${i}`);
+// }
+
