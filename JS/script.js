37da1616d8aafbd6b1a8ce06b1f0036f0e8e8e6c
Konstantin <O_o@iMac-BadBudahh.local>
2021-02-02 15:29:04 +0200

    3d commit

--- JS/script.js
+++ JS/script.js
@@ -1,61 +1,39 @@
 'use strict';
 
-// const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// const personalMoviesDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// };
+const personalMoviesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// const a = prompt('Последний просмотренный вами фильм?', ''),
-//     b = prompt('На сколько вы его оцените', ''),
-//     c = prompt('Последний просмотренный вами фильм?', ''),
-//     d = prompt('На сколько вы его оцените', '');
+    for(let i = 0; i < 2; i++) {
+        const a = prompt('Последний просмотренный вами фильм?', ''),
+        b = prompt('На сколько вы его оцените', '');
 
-//     personalMoviesDB.movies[a] = b
-//     personalMoviesDB.movies[c] = d
+        if(a != null && b != null && a != '' && b != '' && a.length < 50  ) {
+            personalMoviesDB.movies[a] = b
+            // console.log('Вы не ввели название!');
+        } else {
+            console.log('Error');
+            i--;
+        }
 
-// console.log(personalMoviesDB);
+        if (personalMoviesDB.count < 10) {
+            console.log('Просмотрено мало фильмов');
+        } else if (personalMoviesDB.count >= 10 && personalMoviesDB.count <= 30) {
+            console.log('Вы классический зритель');
+        } else {
+            console.log('Да вы киноман батюшка');
+        }
+    
+    }
 
-// const num = 151;
+ 
+    
 
-// (num > 55) ? console.log('true') : console.log('false');
-// ======= Оператор switch
-// switch (num) {
-//     case 49:
-//         console.log('Error');
-//         break;
-//     case 100:
-//         console.log('Bingo')
-//         break;    
-//     case 151:
-//         console.log('Bullseye')
-//         break;
-//     default:
-//         console.log('miss again');        
-// }
-
-// let num = 50;
-
-// while(num < 55) {
-//     console.log(`Cont start: ${num}`);
-//     num++
-// }
-
-// do {
-//         console.log(`Cont start: ${num}`);
-//     num++;
-// } 
-// while (num < 60);
-
-// for(let i = 0; i < 8; i++) {
-//     if(i === 5) {
-//         console.log('Success');
-//         break;
-//     }
-//     console.log(`Nasa hacking: ${i}`);
-// }
+console.log(personalMoviesDB);
 
